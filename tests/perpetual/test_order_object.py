b1fbd45b6cfd245a8e6e360ef4097ade12fc207f
stefano <stefano.franz@me.com>
2024-05-07 09:17:27 +0000

    fix tests

--- tests/perpetual/test_order_object.py
+++ tests/perpetual/test_order_object.py
@@ -12,7 +12,9 @@ FROZEN_NONCE = 1473459052
 
 @freeze_time("2024-01-05 01:08:56.860694")
 @pytest.mark.asyncio
-async def test_create_sell_order(mocker: MockerFixture, create_trading_account, create_btc_usd_market):
+async def test_create_sell_order(
+    mocker: MockerFixture, create_trading_account, create_btc_usd_market
+):
     mocker.patch("x10.utils.starkex.generate_nonce", return_value=FROZEN_NONCE)
 
     from x10.perpetual.order_object import create_order_object
@@ -31,7 +33,7 @@ async def test_create_sell_order(mocker: MockerFixture, create_trading_account, 
         order_obj.to_api_request_json(),
         equal_to(
             {
-                "id": "2057177517787449460689138427731484839396065306136765792011752882862551327344",
+                "id": "3280850248231586971858312197993273142004261856737545220604391622653484552257",
                 "market": "BTC-USD",
                 "type": "LIMIT",
                 "side": "SELL",
@@ -53,8 +55,8 @@ async def test_create_sell_order(mocker: MockerFixture, create_trading_account, 
                 "stopLossLimitPrice": None,
                 "settlement": {
                     "signature": {
-                        "r": "0x4a8c8928a5bd63bf11ed6570d25078ff67a14dfba6a75855cc3b3d19e26621e",
-                        "s": "0x11afcfac10aea315b88f774836e980d81738317156d59d20abf9cd4c374fd77",
+                        "r": "0x6036ac4ade5f5dfb1d293450fbf3a6864be35d3d90f78c61a6e97ed08af60ad",
+                        "s": "0x6d2b1e8e4ce023cb4c85864ac042d865c4e5a455b1916606bbde9386357a9ee",
                     },
                     "starkKey": "0x61c5e7e8339b7d56f197f54ea91b776776690e3232313de0f2ecbd0ef76f466",
                     "collateralPosition": "10002",
@@ -62,7 +64,7 @@ async def test_create_sell_order(mocker: MockerFixture, create_trading_account, 
                 "debuggingAmounts": {
                     "collateralAmount": "43445116",
                     "feeAmount": "21723",
-                    "syntheticAmount": "10000000",
+                    "syntheticAmount": "1000",
                 },
             }
         ),
@@ -71,7 +73,9 @@ async def test_create_sell_order(mocker: MockerFixture, create_trading_account, 
 
 @freeze_time("2024-01-05 01:08:56.860694")
 @pytest.mark.asyncio
-async def test_create_buy_order(mocker: MockerFixture, create_trading_account, create_btc_usd_market):
+async def test_create_buy_order(
+    mocker: MockerFixture, create_trading_account, create_btc_usd_market
+):
     mocker.patch("x10.utils.starkex.generate_nonce", return_value=FROZEN_NONCE)
 
     from x10.perpetual.order_object import create_order_object
@@ -90,7 +94,7 @@ async def test_create_buy_order(mocker: MockerFixture, create_trading_account, c
         order_obj.to_api_request_json(),
         equal_to(
             {
-                "id": "3155583690180525934046012843594580395696986309866824276323868753991017198384",
+                "id": "3341877135169230217913846589832056830347340581639748919090243450434880694177",
                 "market": "BTC-USD",
                 "type": "LIMIT",
                 "side": "BUY",
@@ -112,8 +116,8 @@ async def test_create_buy_order(mocker: MockerFixture, create_trading_account, c
                 "stopLossLimitPrice": None,
                 "settlement": {
                     "signature": {
-                        "r": "0x702fb315701838342b120267a1b0ca62c2dd79ea4fbf5b2faf23985aaf962b2",
-                        "s": "0x388e459d7e04c187d1635fcc3529e139a41e1c9c1364c8707060c3b97adfee",
+                        "r": "0xc68d574e2e92138f799068506a5e2840083ff1a6e3d65498f38e3bf7dd1c95",
+                        "s": "0x5b51dd5664176164dcff0fb7a7eff7db748d920bbc1be4dc42b433286139bfa",
                     },
                     "starkKey": "0x61c5e7e8339b7d56f197f54ea91b776776690e3232313de0f2ecbd0ef76f466",
                     "collateralPosition": "10002",
@@ -121,7 +125,7 @@ async def test_create_buy_order(mocker: MockerFixture, create_trading_account, c
                 "debuggingAmounts": {
                     "collateralAmount": "43445117",
                     "feeAmount": "21723",
-                    "syntheticAmount": "10000000",
+                    "syntheticAmount": "1000",
                 },
             }
         ),
