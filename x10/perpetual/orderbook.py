8b05f90b5403b075120ca00935e03bb11796c044
stefano <stefano.franz@me.com>
2024-06-15 10:37:56 +0000

    add orderbook

---
+++ x10/perpetual/orderbook.py
@@ -0,0 +1,136 @@
+import decimal
+from typing import Callable
+from x10.perpetual.configuration import TESTNET_CONFIG, EndpointConfig
+from x10.perpetual.orderbooks import OrderbookQuantityModel, OrderbookUpdateModel
+from x10.perpetual.stream_client.stream_client import PerpetualStreamClient
+from x10.utils.http import StreamDataType
+from sortedcontainers import SortedDict
+
+import asyncio
+import dataclasses
+
+
+@dataclasses.dataclass
+class OrderBookEntry:
+    price: decimal.Decimal
+    amount: decimal.Decimal
+
+    def __repr__(self) -> str:
+        return f"OrderBookEntry(price={self.price}, amount={self.amount})"
+
+
+class OrderBook:
+    @staticmethod
+    async def create(
+        endpoint_config: EndpointConfig,
+        market_name: str,
+        best_ask_change_callback: Callable[[OrderBookEntry], None] | None = None,
+        best_bid_change_callback: Callable[[OrderBookEntry], None] | None = None,
+        start=False,
+    ) -> "OrderBook":
+        ob = OrderBook(
+            endpoint_config,
+            market_name,
+            best_ask_change_callback,
+            best_bid_change_callback,
+        )
+        if start:
+            await ob.start_orderbook()
+        return ob
+
+    def __init__(
+        self,
+        endpoint_config: EndpointConfig,
+        market_name: str,
+        best_ask_change_callback: Callable[[OrderBookEntry], None] | None = None,
+        best_bid_change_callback: Callable[[OrderBookEntry], None] | None = None,
+    ) -> None:
+        self.__stream_client = PerpetualStreamClient(api_url=endpoint_config.stream_url)
+        self.__market_name = market_name
+        self.__task: asyncio.Task | None = None
+        self._bid_prices: SortedDict[decimal.Decimal, OrderBookEntry] = SortedDict()
+        self._ask_prices: SortedDict[decimal.Decimal, OrderBookEntry] = SortedDict()
+        self.best_ask_change_callback = best_ask_change_callback
+        self.best_bid_change_callback = best_bid_change_callback
+
+    def update_orderbook(self, data: OrderbookUpdateModel):
+            current_best_bid = self.best_bid()
+            for bid in data.bid:
+                if bid.price in self._bid_prices:
+                    existing_entry: OrderBookEntry = self._bid_prices.get(
+                        bid.price
+                    )
+                    existing_entry.amount = existing_entry.amount + bid.qty
+                    if existing_entry.amount == 0:
+                        del self._bid_prices[bid.price]
+                else:
+                    self._bid_prices[bid.price] = OrderBookEntry(
+                        price=decimal.Decimal(bid.price),
+                        amount=decimal.Decimal(bid.qty),
+                    )
+            now_best_bid = self.best_bid()
+            if now_best_bid and current_best_bid != now_best_bid:
+                if self.best_bid_change_callback:
+                    self.best_bid_change_callback(now_best_bid)
+
+            current_best_ask = self.best_ask()
+            for ask in data.ask:
+                if ask.price in self._ask_prices:
+                    existing_entry: OrderBookEntry = self._ask_prices.get(
+                        ask.price
+                    )
+                    existing_entry.amount = existing_entry.amount + ask.qty
+                    if existing_entry.amount == 0:
+                        del self._ask_prices[ask.price]
+                else:
+                    self._ask_prices[ask.price] = OrderBookEntry(
+                        price=decimal.Decimal(ask.price),
+                        amount=decimal.Decimal(ask.qty),
+                    )
+            if current_best_ask != self.best_ask():
+                if self.best_ask_change_callback:
+                    self.best_ask_change_callback(self.best_ask())
+
+    def init_orderbook(self, data: OrderbookUpdateModel):
+        for bid in data.bid:
+            self._bid_prices[bid.price] = OrderBookEntry(
+                price=decimal.Decimal(bid.price),
+                amount=decimal.Decimal(bid.qty),
+            )
+        for ask in data.ask:
+            self._ask_prices[ask.price] = OrderBookEntry(
+                price=decimal.Decimal(ask.price),
+                amount=decimal.Decimal(ask.qty),
+            )
+
+    async def start_orderbook(self) -> asyncio.Task:
+        loop = asyncio.get_running_loop()
+        async def inner():
+            async with self.__stream_client.subscribe_to_orderbooks(
+                self.__market_name
+            ) as stream:
+                async for event in stream:
+                    if event.type == StreamDataType.SNAPSHOT.value:
+                        self.init_orderbook(event.data)
+                    elif event.type == StreamDataType.DELTA.value:
+                        self.update_orderbook(event.data)
+        self.__task = loop.create_task(inner())
+
+    def stop_orderbook(self):
+        if self.__task:
+            self.__task.cancel()
+            self.__task = None
+
+    def best_bid(self) -> OrderBookEntry | None:
+        try:
+            entry = self._bid_prices.peekitem(-1)
+            return entry[1]
+        except IndexError:
+            return None
+
+    def best_ask(self) -> OrderBookEntry | None:
+        try:
+            entry = self._ask_prices.peekitem(0)
+            return entry[1]
+        except IndexError:
+            return None
--- x10/perpetual/simple_client/simple_trading_client.py
+++ x10/perpetual/simple_client/simple_trading_client.py
@@ -1,4 +1,5 @@
 import asyncio
+import dataclasses
 from decimal import Decimal
 from typing import Dict, Tuple, Union
 
@@ -6,7 +7,7 @@ from x10.perpetual.accounts import AccountStreamDataModel, StarkPerpetualAccount
 from x10.perpetual.configuration import EndpointConfig
 from x10.perpetual.markets import MarketModel
 from x10.perpetual.order_object import create_order_object
-from x10.perpetual.orders import OpenOrderModel, OrderSide
+from x10.perpetual.orders import OpenOrderModel, OrderSide, OrderStatus, PerpetualOrderModel
 from x10.perpetual.stream_client.perpetual_stream_connection import (
     PerpetualStreamConnection,
 )
@@ -18,35 +19,51 @@ from x10.perpetual.trading_client.order_management_module import OrderManagement
 from x10.utils.http import WrappedStreamResponse
 
 
+@dataclasses.dataclass
+class OrderWaiter:
+    condition: asyncio.Condition
+    open_order: None | OpenOrderModel
+
+
 class BlockingTradingClient:
     def __init__(self, endpoint_config: EndpointConfig, account: StarkPerpetualAccount):
         self.__endpoint_config = endpoint_config
         self.__account = account
-        self.__market_module = MarketsInformationModule(endpoint_config.api_base_url, account.api_key)
-        self.__orders_module = OrderManagementModule(endpoint_config.api_base_url, account.api_key)
+        self.__market_module = MarketsInformationModule(
+            endpoint_config.api_base_url, account.api_key
+        )
+        self.__orders_module = OrderManagementModule(
+            endpoint_config.api_base_url, account.api_key
+        )
         self.__markets: Union[None, Dict[str, MarketModel]] = None
-        self.__stream_client: PerpetualStreamClient = PerpetualStreamClient(api_url=endpoint_config.stream_url)
+        self.__stream_client: PerpetualStreamClient = PerpetualStreamClient(
+            api_url=endpoint_config.stream_url
+        )
         self.__account_stream: Union[
             None,
             PerpetualStreamConnection[WrappedStreamResponse[AccountStreamDataModel]],
         ] = None
-        self.__order_waiters: Dict[str, Tuple[asyncio.Condition, Union[None, OpenOrderModel]]] = {}
+        self.__order_waiters: Dict[str, OrderWaiter] = {}
         self.__orders_task: Union[None, asyncio.Task] = None
+        self.__stream_lock = asyncio.Lock()
 
     async def ___order_stream(self):
         async for event in self.__account_stream:
             if not (event.data and event.data.orders):
                 continue
-            else:
-                pass
             for order in event.data.orders:
-                (condition, open_order) = self.__order_waiters.get(order.external_id)
-                if not condition:
+                if order.external_id not in self.__order_waiters:
                     continue
-                if condition:
-                    async with condition:
-                        self.__order_waiters[order.external_id] = (condition, order)
-                        condition.notify_all()
+                order_waiter = self.__order_waiters.get(order.external_id)
+                if not order_waiter:
+                    continue
+                if order_waiter.condition:
+                    async with order_waiter.condition:
+                        order_waiter.open_order = order
+                        order_waiter.condition.notify_all()
+
+    async def cancel_order(self, order_id: int):
+        await self.__orders_module.cancel_order(order_id)
 
     async def create_and_place_order(
         self,
@@ -65,10 +82,17 @@ class BlockingTradingClient:
             raise ValueError(f"Market '{market_name}' not found.")
 
         if not self.__account_stream:
-            self.__account_stream = await self.__stream_client.subscribe_to_account_updates(self.__account.api_key)
-            self.__orders_task = asyncio.create_task(self.___order_stream())
+            await self.__stream_lock.acquire()
+            if not self.__account_stream:
+                self.__account_stream = (
+                    await self.__stream_client.subscribe_to_account_updates(
+                        self.__account.api_key
+                    )
+                )
+                self.__orders_task = asyncio.create_task(self.___order_stream())
+            self.__stream_lock.release()
 
-        order = create_order_object(
+        order: PerpetualOrderModel = create_order_object(
             account=self.__account,
             market=market,
             amount_of_synthetic=amount_of_synthetic,
@@ -78,16 +102,21 @@ class BlockingTradingClient:
             previous_order_id=previous_order_id,
         )
 
-        self.__order_waiters[order.id] = (asyncio.Condition(), None)
+        if order.id in self.__order_waiters:
+            raise ValueError(f"order with {order.id} hash already placed")
+
+        self.__order_waiters[order.id] = OrderWaiter(asyncio.Condition(), None)
         placed_order_task = asyncio.create_task(self.__orders_module.place_order(order))
-        (waiting_condition, open_order) = self.__order_waiters[order.id]
-        if open_order:
-            return open_order
-        async with waiting_condition:
+        order_waiter = self.__order_waiters[order.id]
+        if order_waiter.open_order:
+            return order_waiter.open_order
+        async with order_waiter.condition:
             await asyncio.gather(
-                placed_order_task, asyncio.wait_for(waiting_condition.wait(), 5), return_exceptions=False
+                placed_order_task,
+                asyncio.wait_for(order_waiter.condition.wait(), 5),
+                return_exceptions=False,
             )
-            open_model = self.__order_waiters[order.id][1]
+            open_model = self.__order_waiters[order.id].open_order
             del self.__order_waiters[order.id]
             if not open_model:
                 raise ValueError("No Fill or Placement received for order")
