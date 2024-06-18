7a27c8286c22df08f6963575f07327e33fa5eb0b
stefano <stefano.franz@me.com>
2024-06-18 16:54:47 +0000

    persist test data to csv

--- examples/x10_throughput_latency_test.py
+++ examples/x10_throughput_latency_test.py
@@ -7,7 +7,7 @@ import os
 from asyncio import run
 from decimal import Decimal
 from multiprocessing import Process, Queue
-from typing import Optional
+from typing import List, Optional
 
 from dotenv import load_dotenv
 
@@ -20,12 +20,24 @@ from x10.perpetual.trading_client import PerpetualTradingClient
 
 NUM_PRICE_LEVELS = 1
 
+PLACE = "PLACE"
+CANCEL = "CANCEL"
+NANOS_IN_SECOND = 1000 * 1000 * 1000
+
 @dataclasses.dataclass(frozen=True)
 class TimedOperation:
     name: str
     start_nanos: int
     end_nanos: int
     operation_ms: float
+
+@dataclasses.dataclass(frozen=True)
+class TimeSeriesChunk:
+    start_ns: int
+    end_ns: int
+    mean_operation_latency_ms: float
+    std_dev_operation_latency_ms: float
+    throughput: float
 
 
 async def clean_it(trading_client: PerpetualTradingClient, market_name: str | None = None):
@@ -41,7 +53,7 @@ async def clean_it(trading_client: PerpetualTradingClient, market_name: str | No
 async def setup_and_run(base: str = "BTC", queue: Optional[Queue] = None):
     market_name = f"{base}-USD"
     print("Running for market: ", market_name)
-    load_dotenv(f"/workspaces/python_trading_clone/env/{base}.env")
+    load_dotenv(f"./env/{base}.env")
     API_KEY = os.getenv("X10_API_KEY")
     PUBLIC_KEY = os.getenv("X10_PUBLIC_KEY")
     PRIVATE_KEY = os.getenv("X10_PRIVATE_KEY")
@@ -115,17 +127,41 @@ async def setup_and_run(base: str = "BTC", queue: Optional[Queue] = None):
                         side=side,
                         post_only=True,
                     )
-                    queue.put(TimedOperation("place", timed_place.start_nanos, timed_place.end_nanos, timed_place.operation_ms))
+                    queue.put(
+                        TimedOperation(
+                            PLACE,
+                            timed_place.start_nanos,
+                            timed_place.end_nanos,
+                            timed_place.operation_ms,
+                        )
+                    )
                     timed_cancel = await blocking_client.cancel_order(order_id=timed_place.id)
-                    queue.put(TimedOperation("cancel", timed_cancel.start_nanos, timed_cancel.end_nanos, timed_cancel.operation_ms))
+                    queue.put(
+                        TimedOperation(
+                            CANCEL,
+                            timed_cancel.start_nanos,
+                            timed_cancel.end_nanos,
+                            timed_cancel.operation_ms,
+                        )
+                    )
                 else:
                     print("No baseline price for market", market_name)
                     await asyncio.sleep(1)
 
         return asyncio.get_running_loop().create_task(inner())
 
-    sell_tasks = list(map(lambda idx: order_loop(idx, OrderSide.SELL, outbound_queue=queue), range(NUM_PRICE_LEVELS)))
-    buy_tasks = list(map(lambda idx: order_loop(idx, OrderSide.BUY, outbound_queue=queue), range(NUM_PRICE_LEVELS)))
+    sell_tasks = list(
+        map(
+            lambda idx: order_loop(idx, OrderSide.SELL, outbound_queue=queue),
+            range(NUM_PRICE_LEVELS),
+        )
+    )
+    buy_tasks = list(
+        map(
+            lambda idx: order_loop(idx, OrderSide.BUY, outbound_queue=queue),
+            range(NUM_PRICE_LEVELS),
+        )
+    )
 
     for task in sell_tasks:
         print(await task)
@@ -139,6 +175,10 @@ def entry_point(base: str, queue: Queue):
 
 if __name__ == "__main__":
     markets = ["ARB", "AVAX", "BTC", "DOGE", "ETH", "LINK", "SOL", "WLD"]
+    cancels: List[TimedOperation] = []
+    cancels_chunks: List[TimeSeriesChunk] = []
+    places: List[TimedOperation] = []
+    place_chunks: List[TimeSeriesChunk] = []
 
     q = Queue()
     subprocesses = map(lambda market: Process(target=entry_point, args=[market, q]), markets)
@@ -147,18 +187,71 @@ if __name__ == "__main__":
         p.start()
 
     import threading
+    import csv
+    cancel_file = open("cancel.csv", "w")
+    place_file = open("place.csv", "w")
+    cancels_csv = csv.DictWriter(cancel_file, fieldnames=list(TimeSeriesChunk.__annotations__.keys()))
+    places_csv = csv.DictWriter(place_file, fieldnames=list(TimeSeriesChunk.__annotations__.keys()))
+
+    poison_pill = None
+
+    def handle_operation(new_operation: TimedOperation, 
+                         list: List[TimedOperation],
+                         chunks: List[TimeSeriesChunk]) -> TimeSeriesChunk | None:
+        list.append(new_operation)
+        newest = new_operation.end_nanos
+        oldest = list[0].start_nanos
+        if newest - oldest > NANOS_IN_SECOND:
+            latencies = [operation.operation_ms for operation in list]
+            mean_latency = round(sum(latencies) / len(latencies), 1)
+            latency_std_dev = round((sum((x - mean_latency) ** 2 for x in latencies) / len(latencies)) ** 0.5, 1)
+            throughput_per_second = round(len(list) / ((newest - oldest) / NANOS_IN_SECOND), 1)
+            chunk = TimeSeriesChunk(
+                    start_ns=oldest,
+                    end_ns=newest,
+                    mean_operation_latency_ms=mean_latency,
+                    std_dev_operation_latency_ms=latency_std_dev,
+                    throughput=throughput_per_second,
+                )
+            chunks.append(
+                chunk
+            )
+            list.clear()
+            return chunk
+        return None
 
     def read_queue():
+        cancels_csv.writeheader()
+        places_csv.writeheader()
         while True:
-            print(q.get())
+            element: TimedOperation = q.get()
+            if element == poison_pill:
+                break
+            if element.name == PLACE:
+                chunk = handle_operation(element, places, place_chunks)
+                if chunk:
+                    places_csv.writerow(dataclasses.asdict(chunk))
+                    place_file.flush()
+            elif element.name == CANCEL:
+                chunk = handle_operation(element, cancels, cancels_chunks)
+                if chunk:
+                    cancels_csv.writerow(dataclasses.asdict(chunk))
+                    cancel_file.flush()
+        cancel_file.close()
+        place_file.close()
+        print("Exiting queue reader")
 
-    threading.Thread(target=read_queue).start()
+
+
+    queue_reader = threading.Thread(target=read_queue)
+    queue_reader.start()
 
     import signal
     import sys
 
     def signal_handler(sig, frame):
         print("You pressed Ctrl+C!")
+        q.put(None)
         for p in subprocesses:
             p.kill()
         sys.exit(0)
@@ -166,5 +259,3 @@ if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
     print("Press Ctrl+C to exit")
     signal.pause()
-
-    # entry_point("ARB", None)
