7ce30c16636d45c7aff79e1c16af095d99b79050
Alexander Shchekoldin <alex@ex10.org>
2024-07-17 14:18:03 +0400

    Remove unused starkware module code

---
+++ vendor/starkware/README.md
@@ -0,0 +1,3 @@
+# A collection of tools to support the Stark Exchange
+
+https://github.com/starkware-libs/starkex-resources/tree/master
--- vendor/starkware/crypto/signature/fast_pedersen_hash.py
+++ vendor/starkware/crypto/signature/fast_pedersen_hash.py
@@ -1,18 +1,16 @@
 from fastecdsa.curve import Curve
 from fastecdsa.point import Point
 
-from vendor.starkware.crypto.signature.signature import (
+from vendor.starkware.crypto.signature import (
+    ALPHA, BETA, CONSTANT_POINTS, EC_ORDER, FIELD_PRIME, N_ELEMENT_BITS_HASH, SHIFT_POINT)
+
+curve = Curve(
+    'Curve0',
+    FIELD_PRIME,
     ALPHA,
     BETA,
-    CONSTANT_POINTS,
     EC_ORDER,
-    FIELD_PRIME,
-    N_ELEMENT_BITS_HASH,
-    SHIFT_POINT,
-)
-from vendor.starkware.python.utils import from_bytes, to_bytes
-
-curve = Curve("Curve0", FIELD_PRIME, ALPHA, BETA, EC_ORDER, *SHIFT_POINT)
+    *SHIFT_POINT)
 
 LOW_PART_BITS = 248
 LOW_PART_MASK = 2**248 - 1
@@ -23,15 +21,19 @@ P_2 = Point(*CONSTANT_POINTS[2 + N_ELEMENT_BITS_HASH], curve=curve)
 P_3 = Point(*CONSTANT_POINTS[2 + N_ELEMENT_BITS_HASH + LOW_PART_BITS], curve=curve)
 
 
-def process_single_element(element: int, p1, p2) -> Point:
-    assert 0 <= element < FIELD_PRIME, "Element integer value is out of range"
+def process_single_element(element: bytes, p1, p2) -> Point:
+    assert len(element) == 32, 'Unexpected element length'
 
-    high_nibble = element >> LOW_PART_BITS
-    low_part = element & LOW_PART_MASK
+    val = int.from_bytes(element, 'big', signed=False)
+    assert val < EC_ORDER, 'Element int value >= EC_ORDER'
+
+    high_nibble = val >> LOW_PART_BITS
+    low_part = val & LOW_PART_MASK
+
     return low_part * p1 + high_nibble * p2
 
 
-def pedersen_hash(x: int, y: int) -> int:
+def pedersen_hash_func(x: bytes, y: bytes) -> bytes:
     """
     Computes the Starkware version of the Pedersen hash of x and y.
     The hash is defined by:
@@ -39,14 +41,13 @@ def pedersen_hash(x: int, y: int) -> int:
     where x_low is the 248 low bits of x, x_high is the 4 high bits of x and similarly for y.
     shift_point, P_0, P_1, P_2, P_3 are constant points generated from the digits of pi.
     """
-    return (
-        HASH_SHIFT_POINT + process_single_element(x, P_0, P_1) + process_single_element(y, P_2, P_3)
-    ).x
+    return (HASH_SHIFT_POINT + process_single_element(x, P_0, P_1) +
+            process_single_element(y, P_2, P_3)).x.to_bytes(32, 'big')
 
 
-def pedersen_hash_func(x: bytes, y: bytes) -> bytes:
+async def async_pedersen_hash_func(x: bytes, y: bytes) -> bytes:
     """
-    A variant of 'pedersen_hash', where the elements and their resulting hash are in bytes.
+    Async variant of pedersen_hash_func.
     """
-    assert len(x) == len(y) == 32, "Unexpected element length."
-    return to_bytes(pedersen_hash(*(from_bytes(element) for element in (x, y))))
+
+    return pedersen_hash_func(x, y)
