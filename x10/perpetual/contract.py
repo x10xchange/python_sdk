0c3bf04488904dc0b28a084f1c2eaf6d0f5b1fbd
Alexander Shchekoldin <alex@ex10.org>
2024-07-31 21:02:19 +0400

    Slow withdrawals

--- x10/perpetual/contract.py
+++ x10/perpetual/contract.py
@@ -8,20 +8,20 @@ from x10.perpetual.markets import MarketModel
 
 DEFAULT_API_TIMEOUT = 30
 
+ABI_FOLDER = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)),
+    "abi",
+)
 STARK_PERPETUAL_ABI = "stark-perpetual.json"
 
 
 def call_stark_perpetual_withdraw(contract_address: str, eth_address: str, market: MarketModel, config: EndpointConfig):
     web3_provider = Web3.HTTPProvider(config.chain_rpc_url, request_kwargs={"timeout": DEFAULT_API_TIMEOUT})
     web3 = Web3(web3_provider)
-    abi_folder = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-        "abi",
-    )
 
     contract = web3.eth.contract(
         address=Web3.to_checksum_address(contract_address),
-        abi=json.load(open(os.path.join(abi_folder, STARK_PERPETUAL_ABI), "r")),
+        abi=json.load(open(os.path.join(ABI_FOLDER, STARK_PERPETUAL_ABI), "r")),
     )
 
     method = contract.functions.withdraw(
