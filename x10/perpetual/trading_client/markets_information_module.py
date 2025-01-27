df8d6c37e77ca5add88c5d2ff84106bb59d732a4
sergei.bulavintsev <sergei@ex10.org>
2025-01-27 13:30:48 +0000

    Added get orderbook snapshot to market info module

--- x10/perpetual/trading_client/markets_information_module.py
+++ x10/perpetual/trading_client/markets_information_module.py
@@ -4,6 +4,7 @@ from typing import List, Optional
 from x10.perpetual.candles import CandleInterval, CandleModel, CandleType
 from x10.perpetual.funding_rates import FundingRateModel
 from x10.perpetual.markets import MarketModel, MarketStatsModel
+from x10.perpetual.orderbooks import OrderbookUpdateModel
 from x10.perpetual.trading_client.base_module import BaseModule
 from x10.utils.date import to_epoch_millis
 from x10.utils.http import send_get_request
@@ -72,4 +73,4 @@ class MarketsInformationModule(BaseModule):
         """
 
         url = self._get_url("/info/markets/<market>/orderbook", market=market_name)
-        return await send_get_request(await self.get_session(), url, dict)
+        return await send_get_request(await self.get_session(), url, OrderbookUpdateModel)
