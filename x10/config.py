4a19f2fa5cf37a7befb4d8059b8b91a12a69f83f
alex101xela <alex@ex10.org>
2025-10-27 13:39:24 +0400

    Add partial TPSL support

--- x10/config.py
+++ x10/config.py
@@ -1,8 +1,5 @@
 import importlib.metadata
 
-TRADING_API_URL_DEV = "http://api.testnet.extended.exchange/api/v1"
-STREAM_API_URL_DEV = "wss://api.testnet.extended.exchange/stream.extended.exchange/v1"
-
 BTC_USD_MARKET = "BTC-USD"
 SOL_USD_MARKET = "SOL-USD"
 ADA_USD_MARKET = "ADA-USD"
