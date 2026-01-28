5a37ce139191b0ecdccd1fbefc8ad55aacdfb4d7
Mark Epshtein <mark@ex10.org>
2026-01-28 13:09:41 +0000

    Drop api keys from response (#75)

--- x10/perpetual/accounts.py
+++ x10/perpetual/accounts.py
@@ -76,7 +76,6 @@ class AccountModel(X10BaseModel):
     l2_key: str
     l2_vault: int
     bridge_starknet_address: Optional[str] = None
-    api_keys: Optional[List[str]] = None
 
 
 class ApiKeyResponseModel(X10BaseModel):
