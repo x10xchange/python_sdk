46e5fcd241fa7ddde3d0ce7c1bc9878658c351bf
alex101xela <alex@ex10.org>
2026-03-27 17:18:45 +0400

    Support MARKET order type

--- tests/perpetual/order_object/test_limit_order_object.py
+++ tests/perpetual/order_object/test_limit_order_object.py
@@ -12,7 +12,6 @@ from x10.perpetual.orders import (
     OrderSide,
     OrderTpslType,
     OrderTriggerPriceType,
-    OrderType,
     SelfTradeProtectionLevel,
 )
 from x10.utils.date import utc_now
