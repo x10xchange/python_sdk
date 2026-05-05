29dc801de6797fa3c5f96c27c23d95d3b52f1199
alex101xela <alex@ex10.org>
2026-05-05 19:00:16 +0400

    Refactor user client

--- tests/signing/test_onboarding.py
+++ tests/signing/test_onboarding.py
@@ -12,6 +12,10 @@ from x10.signing.onboarding import (
 )
 from x10.utils.date import utc_now
 
+# All known values from authentication service tests are used.
+KNOWN_L2_PRIVATE_KEY = "0x7dbb2c8651cc40e1d0d60b45eb52039f317a8aa82798bda52eee272136c0c44"
+KNOWN_L2_PUBLIC_KEY = "0x78298687996aff29a0bbcb994e1305db082d084f85ec38bb78c41e6787740ec"
+
 
 @freeze_time("2024-01-05 01:08:56.860694")
 def test_sign_api_request(get_eth_private_key):
@@ -31,10 +35,6 @@ def test_sign_api_request(get_eth_private_key):
 
 @freeze_time("2024-07-30 16:01:02.000000")
 def test_onboarding_object_generation(get_eth_private_key):
-    """
-    All known values from authentication service tests are used.
-    """
-    known_l2_public_key = "0x78298687996aff29a0bbcb994e1305db082d084f85ec38bb78c41e6787740ec"
     l1_account = Account.from_key(get_eth_private_key())
 
     def sign_message(msg: SignableMessage) -> str:
@@ -58,7 +58,7 @@ def test_onboarding_object_generation(get_eth_private_key):
         equal_to(
             {
                 "l1Signature": "9a59eb699eb58f2ec975455f33dd7205c8a569f7b6d7647c25b71e7ab7eec3d30f2b8c9038f06f077167eb90e0c002602e4ecbab180fad4b2c91d2259883e6571c",  # noqa: E501
-                "l2Key": known_l2_public_key,
+                "l2Key": KNOWN_L2_PUBLIC_KEY,
                 "l2Signature": {
                     "r": "0x70881694c59c7212b1a47fbbc07df4d32678f0326f778861ec3a2a5dbc09157",
                     "s": "0x558805193faa5d780719cba5f699ae1c888eec1fee23da4215fdd94a744d2cb",
@@ -78,9 +78,6 @@ def test_onboarding_object_generation(get_eth_private_key):
 
 
 def test_known_l2_accounts(get_eth_private_key):
-    known_l2_private_key = "0x7dbb2c8651cc40e1d0d60b45eb52039f317a8aa82798bda52eee272136c0c44"
-    known_l2_public_key = "0x78298687996aff29a0bbcb994e1305db082d084f85ec38bb78c41e6787740ec"
-
     local_account: LocalAccount = Account.from_key(get_eth_private_key())
     derived_keys = get_l2_keys_from_l1_account(
         account_index=0,
@@ -89,5 +86,5 @@ def test_known_l2_accounts(get_eth_private_key):
         sign_message=lambda msg: local_account.sign_message(msg).signature.hex(),
     )
 
-    assert_that(derived_keys.private_hex, equal_to(known_l2_private_key))
-    assert_that(derived_keys.public_hex, equal_to(known_l2_public_key))
+    assert_that(derived_keys.private_hex, equal_to(KNOWN_L2_PRIVATE_KEY))
+    assert_that(derived_keys.public_hex, equal_to(KNOWN_L2_PUBLIC_KEY))
